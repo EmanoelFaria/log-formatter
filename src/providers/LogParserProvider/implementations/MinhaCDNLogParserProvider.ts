7c09b6235c2ebb2f5f22af72a8aa98ee4fa6e513
Emanoel <emanoel.dev@gmail.com>
2022-06-23 02:47:21 -0300

    refactor: remove methods from minhacdn parser providers and interface because its now responsible of log formatter providers to do

--- src/providers/LogParserProvider/implementations/MinhaCDNLogParserProvider.ts
+++ src/providers/LogParserProvider/implementations/MinhaCDNLogParserProvider.ts
@@ -1,30 +1,9 @@
 import { IAgoraFormattedLine } from '../../../dtos/IAgoraFormattedLog';
 import { IPossibleCommandLineArgs } from '../../../dtos/IPossibleCommandLineArgs';
-import { ECacheStatusTypes } from '../enums/ECacheStatusTypes';
+import { ECacheStatusTypes } from '../../../enums/ECacheStatusTypes';
 import ILogParserProvider from '../models/ILogParserProvider';
 
 export default class MinhaCDNLogParserProvider implements ILogParserProvider {
-  getHeader(possibleOptions?: IPossibleCommandLineArgs): string {
-    const headerVersion = possibleOptions?.version
-      ? possibleOptions.version
-      : '1.0';
-    const headerDate = this.formatDate();
-
-    return [
-      `#Version: ${headerVersion}\n`,
-      `#Date: ${headerDate}\n`,
-      `#Fields: provider http-method status-code uri-path response-size cache-status \n`,
-    ].join('');
-  }
-
-  private formatDate(date?: Date) {
-    date = date ? new Date(date) : new Date();
-
-    const data = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-    const time = `${date.getHours()}:${date.getMinutes()}:${date.getMilliseconds()}`;
-    return `${data} ${time}`;
-  }
-
   parseLine(customLine: string): IAgoraFormattedLine | null {
     const splittedLog = customLine.split('|');
 
@@ -52,16 +31,6 @@ export default class MinhaCDNLogParserProvider implements ILogParserProvider {
       responseSize,
       cacheStatus,
     };
-  }
-
-  mergeLines(lines: string[]): string {
-    return lines.join('');
-  }
-
-  parseValuesToLines(lines: IAgoraFormattedLine[]): string[] {
-    return lines.map(line => {
-      return `"${line.provider}" ${line.httpMethod} ${line.statusCode} ${line.uriPath} ${line.timeTaken} ${line.responseSize} ${line.cacheStatus} \n`;
-    });
   }
 
   parseLines(customLines: string[]): IAgoraFormattedLine[] {
--- src/providers/LogParserProvider/models/ILogParserProvider.ts
+++ src/providers/LogParserProvider/models/ILogParserProvider.ts
@@ -1,13 +1,9 @@
 import { IAgoraFormattedLine } from '../../../dtos/IAgoraFormattedLog';
-import { IPossibleCommandLineArgs } from '../../../dtos/IPossibleCommandLineArgs';
 
 export default interface ILogParserProvider {
-  getHeader(possibleOptions?: IPossibleCommandLineArgs): string;
   parseLine(customLine: string): IAgoraFormattedLine | null | string;
   parseLines(customLines: string[]): IAgoraFormattedLine[];
-  parseValuesToLines(lines: IAgoraFormattedLine[]): string[];
   parseResponse(response: any): any;
-  mergeLines(lines: string[]): string;
   parseChunkToLines(chunk: string): string[];
   chunkHasManyLines(chunk: string): boolean;
 }
