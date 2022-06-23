08f39468ead6cd7152a7e90262d37c849b286e76
Emanoel <emanoel.dev@gmail.com>
2022-06-23 02:49:11 -0300

    test: refactor minhacdn parser provider tests removed deprecated methods

--- src/__tests__/MinhaCDNLogParserProvider.test.ts
+++ src/__tests__/MinhaCDNLogParserProvider.test.ts
@@ -1,4 +1,4 @@
-import { ECacheStatusTypes } from '../providers/LogParserProvider/enums/ECacheStatusTypes';
+import { ECacheStatusTypes } from '../enums/ECacheStatusTypes';
 import MinhaCDNLogParserProvider from '../providers/LogParserProvider/implementations/MinhaCDNLogParserProvider';
 const logParserProvider = new MinhaCDNLogParserProvider();
 import { validateSchema } from './utils';
@@ -231,62 +231,6 @@ describe('Unit tests for implementation of MinhaCDNLogParserProvider', () => {
       } catch (error: any) {
         expect(error.message).toBe('Invalid response from "MINHA CDN" API');
       }
-    });
-  });
-
-  describe('parseValuesToLines Method', () => {
-    it('Should return a valid data from response object ', async () => {
-      const agoraFormattedLine = [
-        {
-          provider: 'MINHA CDN',
-          httpMethod: 'GET',
-          statusCode: 200,
-          uriPath: '/robots.txt',
-          timeTaken: 13,
-          responseSize: 444,
-          cacheStatus: 'HIT',
-        },
-        {
-          provider: 'MINHA CDN',
-          httpMethod: 'GET',
-          statusCode: 401,
-          uriPath: '/robots.txt',
-          timeTaken: 13,
-          responseSize: 10,
-          cacheStatus: 'HIT',
-        },
-      ];
-
-      const [firstLine, secondLine] =
-        logParserProvider.parseValuesToLines(agoraFormattedLine);
-
-      expect(firstLine).toBe(`"MINHA CDN" GET 200 /robots.txt 13 444 HIT \n`);
-      expect(secondLine).toBe(`"MINHA CDN" GET 401 /robots.txt 13 10 HIT \n`);
-    });
-  });
-
-  describe('mergeLines Method', () => {
-    it('Should return a merged string from given string array ', async () => {
-      const stringLines = [
-        `"MINHA CDN" GET 200 /robots.txt 13 444 HIT \n`,
-        `"MINHA CDN" GET 401 /robots.txt 13 10 HIT \n`,
-      ];
-
-      const mergedString = logParserProvider.mergeLines(stringLines);
-
-      expect(mergedString).toBe(
-        `"MINHA CDN" GET 200 /robots.txt 13 444 HIT \n"MINHA CDN" GET 401 /robots.txt 13 10 HIT \n`,
-      );
-    });
-
-    it('Should return a merged string from given string array ', async () => {
-      const stringLines = [`"MINHA CDN" GET 200 /robots.txt 13 444 HIT \n`];
-
-      const mergedString = logParserProvider.mergeLines(stringLines);
-
-      expect(mergedString).toBe(
-        `"MINHA CDN" GET 200 /robots.txt 13 444 HIT \n`,
-      );
     });
   });
 });
