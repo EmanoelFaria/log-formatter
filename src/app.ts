d6c613498fc0efb11b3dabf7cb51f64e1dd25c62
Emanoel <emanoel.dev@gmail.com>
2022-06-22 23:58:54 -0300

    refactor: conversion service to deal with possible args - new  header doc write - and new errors validations

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,22 @@
 
 import RequestProvider from './providers/RequestProvider';
 import CommandLineProvider from './providers/CommandLineProvider';
+import FileManagerProvider from './providers/FileManagerProvider';
 import ConvertLogService from './services/ConvertLogService';
 import * as process from 'process';
 
 const convertLogService = new ConvertLogService(
   RequestProvider,
   CommandLineProvider,
+  FileManagerProvider,
 );
 
-convertLogService.convert().then(() => {
-  console.log('Conversion finished');
-  process.kill(process.pid, 'SIGTERM');
-});
+convertLogService
+  .convert()
+  .then(() => {
+    console.log('Conversion finished');
+    process.kill(process.pid, 'SIGTERM');
+  })
+  .catch((error: any) => {
+    console.log(`[ERROR] ${error.message}`);
+  });
--- src/services/ConvertLogService.ts
+++ src/services/ConvertLogService.ts
@@ -1,40 +1,66 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
 import { promisify } from 'util';
 import LogParserProviderFactory from '../factories/LogParserProviderFactory';
 import ICommandLineProvider from '../providers/CommandLineProvider/models/ICommandLineProvider';
 import ILogParserProvider from '../providers/LogParserProvider/models/ILogParserProvider';
 import IRequestProvider from '../providers/RequestProvider/models/IRequestProvider';
+import IFileManagerProvider from '../providers/FileManagerProvider/models/IFileManagerProvider';
 import { pipeline, Transform } from 'stream';
+import { IPossibleCommandLineArgs } from '../dtos/IPossibleCommandLineArgs';
 const pipelineAsync = promisify(pipeline);
 
 export default class ConvertLogService {
   requestProvider: IRequestProvider;
   logParserProvider: ILogParserProvider;
   commandLineProvider: ICommandLineProvider;
+  fileManagerProvider: IFileManagerProvider;
+  conversionOptions: IPossibleCommandLineArgs;
 
   constructor(
     requestProvider: IRequestProvider,
     commandLineProvider: ICommandLineProvider,
+    fileManagerProvider: IFileManagerProvider,
   ) {
     this.requestProvider = requestProvider;
     this.commandLineProvider = commandLineProvider;
+    this.fileManagerProvider = fileManagerProvider;
+    this.conversionOptions = this.getOptions();
     this.logParserProvider = LogParserProviderFactory.getLogParserInstance(
-      this.getClientName(),
+      this.conversionOptions.origin,
     );
   }
 
-  async convert() {
-    const url = this.getUrl();
-    const outputPath = this.getOutputPath();
-    const writeableStream = fs.createWriteStream(outputPath);
-    const readableStream = await this.getLogData(url);
-
-    await pipelineAsync(readableStream, this.parseChunk(), writeableStream);
+  getOptions(): IPossibleCommandLineArgs {
+    return this.commandLineProvider.getPossibleArgs();
   }
 
-  parseChunk() {
+  async convert() {
+    this.validateArgs();
+    const url = this.getUrl();
+    const outputPath = this.getOutputPath();
+    const writeableStream = this.getWritableStream(outputPath);
+    const readableStream = await this.getLogData(url);
+
+    writeableStream.write(this.getHeader());
+
+    await pipelineAsync(
+      readableStream,
+      this.parseChunkLines(),
+      writeableStream,
+    );
+  }
+
+  getWritableStream(outputPath: string): NodeJS.WritableStream {
+    return this.fileManagerProvider.getWritableStream(outputPath);
+  }
+
+  getHeader() {
+    console.log(this.conversionOptions);
+    return this.logParserProvider.getHeader(this.conversionOptions);
+  }
+
+  parseChunkLines() {
     return new Transform({
       transform: async (chunk, encoding, cb) => {
         const lines = this.logParserProvider.parseChunkToLines(chunk);
@@ -48,35 +74,45 @@ export default class ConvertLogService {
     });
   }
 
-  getClientName(): string | undefined {
-    const clientName = this.commandLineProvider.getArgs()['name'];
-    return clientName;
+  async getLogData(url: string) {
+    try {
+      const response = await this.requestProvider.get(url);
+      const parsedResponse = this.logParserProvider.parseResponse(response);
+      return parsedResponse;
+    } catch (error) {
+      throw new Error('Error while getting logs from origin');
+    }
   }
 
-  async getLogData(url: string) {
-    const response = await this.requestProvider.get(url);
-    const parsedResponse = this.logParserProvider.parseResponse(response);
-    return parsedResponse;
+  validateArgs() {
+    const args = this.commandLineProvider.getArrayArgs();
+    if (args.length != 2) throw new Error(`Origin and destiny are required`);
   }
 
   getUrl(): string {
     const args = this.commandLineProvider.getArrayArgs();
-    this.commandLineProvider.getArgs();
 
     const url = args[0];
 
-    if (!url) throw new Error('Invalid url param');
+    this.validateUrl(url);
 
     return url;
   }
 
+  validateUrl(url: string) {
+    try {
+      new URL(url);
+    } catch (_) {
+      throw new Error('Origin URL is not valid');
+    }
+  }
+
   getOutputPath(): string {
     const args = this.commandLineProvider.getArrayArgs();
-    this.commandLineProvider.getArgs();
 
     const url = args[1];
 
-    if (!url) throw new Error('Invalid url param');
+    if (!url) throw new Error('Invalid destination param');
 
     return url;
   }
